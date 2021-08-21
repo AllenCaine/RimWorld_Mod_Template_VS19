b81c5324ad70b3806d69aff088099694cf351fd2
Allen_Caine <jonas.stark@mail.de>
2021-08-21 23:15:12 +0200

    Add project files.

---
+++ RimWorld_Mod_Template_VS19/src/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ModName
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+            Log.Message("ExampleModLoaded!");
+        }
+    }
+}
